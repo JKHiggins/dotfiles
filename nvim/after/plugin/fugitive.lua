c109f5308750382f83106e071ffb123563e21ed2
Jayce Higgins <jayce.higgins@kelyntech.com>
2025-07-09 13:18:14 -0500

    ✨ (Git): Add fugitive hotkeys for resolving merges

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+vim.keymap.set('n', 'gh', '<cmd>diffget //2<cr>', { desc = 'Get the hunk in the left' })
+vim.keymap.set('n', 'gl', '<cmd>diffget //3<cr>', { desc = 'Get the hunk in the right' })
